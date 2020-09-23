75296942fe35e7451fc698b962809777d3a01c64
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-09-23 15:11:33 -0400

    Add project files.

---
+++ Lab15.3DesigningAndCreatingARestAPI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace Lab15._3DesigningAndCreatingARestAPI.Controllers
+{
+    [Route("Products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IDbConnection Connection()
+        {
+            return new SqlConnection("Server=HN78Q13\\SQLEXPRESS;Database=DapperOverflow;user id=testuser;password=abc123");
+        }
+    }
+}
